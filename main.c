e8863708635ce76c0c28be480cfceb496fd8876d
mrgreenapple24 <mrchill2405@gmail.com>
2025-11-08 17:46:37 +0530

    initialize function updated

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include "defs.h"
 
 void initialize() {
+    int i;
     // Initialize registers and memory once
     pc = 0x200; // pc starts at this address
     opcode = 0; // reset current opcode
     I = 0; // reset index register
     sp = 0; // reset stack pointer
-
+    
+    memset(gfx, 0, sizeof(uint8_t) * 64 * 32);
     // clear display
+    memset(stack, 0, sizeof(uint16_t) * 16);
     // clear stack
+    memset(V, 0, sizeof(uint8_t) * 16):
     // clear registers V0-VF
+    memset(memory, 0, sizeof(uint8_t) * 4096);
     // clear memory
+    memset(key, 0, sizeof(uint8_t) * 16);
 
 
     // load fontset
@@ -19,14 +25,26 @@ void initialize() {
     }
 
     // reset timers
+    delay_timer = 0;
+    sound_timer = 0;
+    srand(time(NULL));
 }
 
 
 
-void loadGame() {
-    for (int i = 0; i < bufferSize; i++) {
-        memory[i+512] = buffer[i];
+void loadGame(char *game) {
+    FILE *fgame;
+
+    fgame = fopen(game, "rb");
+
+    if (fgame == NULL) {
+      fprintf(stderr, "Unable to open game: %s\n", game);
+      exit(42);
     }
+
+    fread(&memory[0x200], 1, MAX_GAME_SIZE, fgame);
+    
+    fclose(fgame);
 }
 
 int main(int argc, char **argv) {
